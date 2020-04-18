842377808c4e3ca5b9d6e5035665d6c4bfc73e8c
tcrct <tcrct@163.com>
2020-04-18 17:42:16 +0800

    待续开发

--- src/main/java/com/duangframework/cache/ehcache/EhCache.java
+++ src/main/java/com/duangframework/cache/ehcache/EhCache.java
@@ -1,8 +1,9 @@
 package com.duangframework.cache.ehcache;
 
+import com.duangframework.cache.core.CacheModel;
 import com.duangframework.cache.core.ICache;
 
-public class EhCache implements ICache {
+public class EhCache implements ICache<EhCache> {
 
     public static final EhCache EH_CACHE = new EhCache();
 
@@ -15,12 +16,17 @@ public class EhCache implements ICache {
     }
 
     @Override
+    public EhCache model(CacheModel model) {
+        return null;
+    }
+
+    @Override
     public <T> T get() {
         return null;
     }
 
     @Override
-    public boolean remove() {
-        return false;
+    public long remove() {
+        return 1;
     }
 }
